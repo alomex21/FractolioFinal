9f07d8d4c870d8e25223b4deb3c171e3461f5725
Clemente <spoerer40@gmail.com>
2023-08-23 19:09:40 -0400

    Fixes

--- lib/views/edit_profile.dart
+++ lib/views/edit_profile.dart
@@ -1,12 +1,114 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
 
-class EditProfile extends StatelessWidget {
+class EditProfile extends StatefulWidget {
   const EditProfile({super.key});
 
   @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  String _newpassword = '', _oldpassword = '', _finalpassword = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("test"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reset Password'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: _logout,
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const Text(
+              'Reset Password',
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 24,
+                  fontStyle: FontStyle.normal),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+              alignment: AlignmentDirectional.center,
+              width: double.infinity,
+              height: 200,
+              child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      color: Color(0x00f1f4f8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x33000000),
+                          blurRadius: 3,
+                          offset: Offset(0, 1),
+                        ),
+                      ]),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Enter old Password'),
+                        obscureText: true,
+                        onSaved: (String? value) => _oldpassword = value!,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Enter new Password'),
+                        obscureText: true,
+                        onSaved: (String? value) => _newpassword,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Confirm your Password'),
+                        obscureText: true,
+                        validator: (String? value) {
+                          if (_newpassword != _finalpassword) {
+                            return 'Please enter the same password';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) => _finalpassword,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _logout() async {
+    await _auth.signOut();
+    if (!context.mounted) return;
+    Navigator.of(context).pushReplacementNamed(
+      '/',
+    );
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Successfully signed out!'),
+      ),
     );
   }
 }
--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fractoliotesting/views/edit_profile.dart';
+import 'package:fractoliotesting/widgets/widgets.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -51,67 +51,5 @@ class ProfileScreen extends StatelessWidget {
 
   void _didPushButton(BuildContext context) {
     Navigator.of(context).pushReplacementNamed('/navigator');
-  }
-}
-
-class BotonPerfil extends StatelessWidget {
-  final String text;
-  final VoidCallback? onTap;
-  const BotonPerfil({
-    super.key,
-    required this.text,
-    this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          height: 60,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            border: Border.all(width: 0, color: const Color(0x00ffffff)),
-            color: Colors.white,
-            boxShadow: const [
-              BoxShadow(
-                color: Color(0x33000000),
-                blurRadius: 3,
-                offset: Offset(0, 1),
-              ),
-            ],
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.fromLTRB(16, 0, 4, 0),
-                child: EditText(text),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class EditText extends StatelessWidget {
-  final String displayText;
-  const EditText(this.displayText, {Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      displayText,
-      style: const TextStyle(
-          fontWeight: FontWeight.w400,
-          fontSize: 16,
-          fontStyle: FontStyle.normal),
-    );
   }
 }
