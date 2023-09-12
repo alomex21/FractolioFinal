43e4d139495a01ae1a84dd53396ffe1aabb4c20b
Clemente <spoerer40@gmail.com>
2023-09-12 18:45:04 -0300

    productpagerefactoring

--- lib/views/product_page.dart
+++ lib/views/product_page.dart
@@ -52,7 +52,7 @@ class _ProductPageState extends State<ProductPage> {
   }
 } */
 
-class ProductsDetail extends StatelessWidget {
+/* class ProductsDetail extends StatelessWidget {
   const ProductsDetail({super.key, required this.productId});
   final String? productId;
 
@@ -137,6 +137,101 @@ class ProductsDetail extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+ */
+
+class ProductsDetail extends StatelessWidget {
+  const ProductsDetail({Key? key, required this.productId}) : super(key: key);
+  final String? productId;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<DocumentSnapshot>(
+      stream: getProductStream(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return const Text('Product not found');
+        }
+
+        return buildProductDetail(snapshot);
+      },
+    );
+  }
+
+  Stream<DocumentSnapshot> getProductStream() {
+    return FirebaseFirestore.instance
+        .collection('Products')
+        .doc(productId ?? '0R2bSVi2Dy6NmME4i3kN')
+        .snapshots();
+  }
+
+  Widget buildProductDetail(AsyncSnapshot<DocumentSnapshot> snapshot) {
+    final Map<String, dynamic> data =
+        snapshot.data!.data() as Map<String, dynamic>;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(data["product_name"] ?? "Default Product Name"),
+      ),
+      body: ListView(
+        children: [
+          buildListTile('Product Name', data["product_name"]),
+          buildListTile('Description', data["description"]),
+          buildListTile('Image URL', data["image_url"]),
+          ListTile(
+            title: const Text('Allergens'),
+            subtitle: buildAllergens(data["allergens"]),
+          ),
+          ListTile(
+            title: const Text('Nutritional Values'),
+            subtitle: buildNutritionalValues(data["nutritional_values"]),
+          ),
+        ],
+      ),
+    );
+  }
+
+  ListTile buildListTile(String title, String? value) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(value ?? ""),
+    );
+  }
+
+  Widget buildNutritionalValues(Map<String, dynamic>? nutritionalValues) {
+    return Row(
+      children: nutritionalValues?.entries.map((entry) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Column(
+                children: [
+                  Text("${entry.key}: ${entry.value.toString()}"),
+                ],
+              ),
+            );
+          }).toList() ??
+          [],
+    );
+  }
+
+  Widget buildAllergens(List? allergens) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: allergens?.map((allergen) {
+            return Text(allergen.toString());
+          }).toList() ??
+          [],
     );
   }
 }
