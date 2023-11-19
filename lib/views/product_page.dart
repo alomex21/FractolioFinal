f8866fd51cf24f2ccd513d2420225e4b5a51a393
Clemente <spoerer40@gmail.com>
2023-11-19 14:36:37 -0300

    icons

--- lib/views/product_page.dart
+++ lib/views/product_page.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:fractoliotesting/views/navigator_logic.dart';
 import 'package:string_capitalize/string_capitalize.dart';
 import '../services/services/firestore_service.dart';
 import 'product_review.dart';
